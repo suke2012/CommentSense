fd48754a55e4eb8d450c6f024d22b807d3d16097
maoli <maolitz2012@sina.com>
2023-12-22 00:32:46 +0800

    feature 1 engineer AI model build

---
+++ src/main/java/com/ai/model/aiprocesscenter/AiProcessCenterApplication.java
@@ -0,0 +1,13 @@
+package com.ai.model.aiprocesscenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProcessCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiProcessCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ai/model/aiprocesscenter/AiProcessCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.model.aiprocesscenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProcessCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
